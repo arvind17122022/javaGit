0f42d00becdfa6af85d882a5c180b439ce00058b
marsarvind <marsarvind@gmail.com>
2025-09-07 18:19:56 +0530

    rev java proram

---
+++ rev.java
@@ -0,0 +1,15 @@
+public class rev {
+
+    public static void main(String[] args) {
+        // Declare variables before using them
+        String real = "Jenkins";   // original string
+        String rev = "";           // will hold the reversed string
+
+        // Reverse string using loop
+        for (int i = real.length() - 1; i >= 0; i--) {
+            rev = rev + real.charAt(i);
+        }
+
+        System.out.println("Reversed string = " + rev);
+    }
+}
